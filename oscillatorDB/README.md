9717d0b785ca6f66e084205e3628593c334c7805
Wesley <wesleyluk.111@gmail.com>
2021-11-07 20:39:24 -0800

    Removed oscillatorDB .git folder

---
+++ oscillatorDB/README.md
@@ -0,0 +1,150 @@
+# oscillatorDB
+
+## Set Up
+Clone this repository:
+```git clone https://github.com/really-lilly/oscillatorDB.git```
+
+This module requires:
+* PyMongo
+* dnsPython
+* tellurium
+
+These can be installed via pip or conda. Alternatively, environment.yml is a conda environment containing all the necessary packages. To use it:
+```
+conda env create -f environment.yml
+conda activate oscillatorDB
+ ```
+ DO NOT INSTALL bson into this environment. It will break everything and you'll waste an entire day trying to fix it and then just end up re-cloning the repo. Trust me, I know. Don't think it will be a good idea to back up the database and google how to do it. Don't implent the accepted answer on StackOverflow. Don't mess with the environment. Do not do what I have done.
+
+You think that doesn't sound that bad? Well you also have to delete the environment and then rebuild it. And then reset your python interpreter to the new environment. Just don't do that. I'll fix it I swear.
+
+
+## Database Description
+
+The data base stores:
+* ID: model's ID number (str)
+* num_nodes: number of species (int)
+* num_reactions: number of reactions (int)
+* model: antimony string for the model
+* oscillator: If the model oscillates or not (boolean). True if does, False if it does not. 
+    * Models are filtered so none go to infinity
+* mass_conserved: True if there are no reactions that violate mass conservation (eg. A + B -> A). False otherwise
+
+
+## Queries
+
+### Simple Queries
+Queries are specified by a dictionary containing the traits of interest. There are two query helper methods in the module to get a list of IDs or the antimony strings of models that match the query. 
+
+#### Get IDs
+```
+import mongoMethods as mm
+
+# Get IDs of oscillators with 3 nodes
+query = { "num_nodes" : 3, "oscillator" : True)}
+model_IDS = mm.get_ids(query)
+```
+#### Get antimony strings
+```
+import mongoMethods as mm
+
+# Get antimony string for model 12345
+query = { "ID" : "1234" }
+ant = mm.get_antimony(query)
+```
+#### Get SBML files
+```
+import mongoMethods as mm
+
+# Get SBML files for all oscillating 3-node models:
+query = {"num_nodes" : 3, "oscillator" True}
+path = "~/path/to/SBML_directory"  # Directory to store SBML files (will be created if it doesn't exist already)
+mm.get_sbml(query, path)
+```
+
+### Custom Queries
+More general queries can be made with the function ```query_database()``` which accepts a query dictionary and returns a cursor object containing the dictionaries for all matching entries. The cursor object can be accessed and interated over as if it were a list of dictionaries.
+
+```
+import mongoMethods as mm
+
+# Get a list of all models with 3 nodes
+query = { 'num_nodes' : 3 }
+models = mm.query_database(query)
+
+# Get the ID of the first model
+ID = models[0]['ID']
+
+# Print the number of reactions in each model
+for model in models:
+    print(model['num_reactions'])
+```
+
+The collection can also be directly queried after directly connecting to the database collection
+```
+import mongoMethods as mm
+
+# Connect to the database collection
+collection = mm.collection
+
+# Get oscillators with 3 nodes and 5 reactions
+query = { "num_nodes" : 3, "num_reactions" : 5, "oscillator" : True }
+entries = collection.find(query)
+
+# Get the IDs of models that match the query
+for entry in entries:
+    print(entry['ID'])
+```
+
+## Adding to Database
+
+New models can be added en masse provided that all antimony files are stored in a single folder. You will also need to provide the oscillation status (True, False). The number of nodes/species will automatically be counted provided that the antimony file starts with listing the species as shown below. 
+'''
+var S0
+var S1
+#etc
+'''
+This will also work if the first line is a comment. If your antimony file does not have this information, the number of nodes can be manually provided with the option argument "num_nodes"
+
+**NOTE: There is currently no safeguard to prevent adding duplicate models to the database. Running the same add_many command twice will duplicate the models previously added.**
+
+##### EXAMPLE: 
+Add oscillating networks from the folder ant_folder
+```
+import mongoMethods as mm
+
+
+path = "/home/user/ant_folder"
+
+# True indicates oscillating model
+mm.add_many(path, True, num_nodes=3)   #add_many(path, oscillator, massConserved, num_nodes=None)
+```
+
+It may be necessary to delete models from the database if they've been erroneously added. **Be catious**, deleting models is permanent and effects the entire database.
+
+##### EXAMPLE:
+Let's say that in the previous example, we added antimony models to the database from the folder ant_folders, but they were erroneously added as oscillators when they are not. We can delete all the models that are stored in the local folder from the database and then re-add them with the correct oscillation status.
+```
+import mongoMethods as mm
+
+connection = mm.get_connection()
+path = "C:\\Users\\user\\ant_folder"
+
+# Remove the models
+mm.delete_by_path(path)
+
+# Re-add the models with the correct oscillator status
+mm.add_many(path, False)
+```
+Models can also be deleted by query. This is useful if you want to delete a specific model. For example, the model 12345 can be deleted by
+```mm.delete_by_query({ 'ID' : '12345' })```
+Please be careful when deleting by query. If your query is not specific enough, you may end up permanently deleting other models from the database as well.
+
+## What's in the database
+Last updated 2021-07-15
+
+* 10 node oscillators
+* 10 node controls (non-oscillators)
+* 3 node oscillators
+* 3 node controls
+
---
+++ oscillatorDB/mongoMethods.py
@@ -0,0 +1,360 @@
+import os
+from pymongo import MongoClient
+import warnings
+from random import randrange
+import tellurium as te
+
+###Fixes the pymongo.errors.ServerSelectionTimeoutError => references installed certificate authority bundle
+import certifi
+ca = certifi.where()
+
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+###Initialization of variables
+astr = ""
+client = ""
+database_names = ""
+db = ""
+collection = ""
+cur = ""
+
+###Startup function so we don't autmoatically call upon the db
+# user: data
+# pwd:  VuRWQ
+def startup():
+    global astr
+    global client
+    global database_names
+    global db
+    global collection
+    global cur
+    astr = "mongodb+srv://data:VuRWQ@networks.wqx1t.mongodb.net"
+    #client = MongoClient(astr)
+    client = MongoClient(astr, tlsCAFile=ca) ###fixes the pymongo.errors.ServerSelectionTimeoutError
+    database_names = client.list_database_names()
+    db = client['networks']
+    db = client.networks
+    collection = db['networks']
+    cur = collection.find({})
+
+
+def print_entries(cursor=cur, n=None):
+    '''
+    Prints entries in the database
+    :param n: optional, print out the first n entries. By default n is none and all entries are printed.
+    :return: print out of every entry dictionary
+    '''
+    print(f"There are {cursor.count()} entries in the database")
+    if not n:
+        for doc in cursor:
+            print(doc)
+    else:
+        count = 1
+        for doc in cursor:
+            print(doc)
+            if count < n:
+                count += 1
+            else:
+                break
+
+
+def get_connection():
+    '''
+    Connect to the mongoDB
+    :return: a MongoClient object connected to the database
+    '''
+    return MongoClient("mongodb+srv://data:VuRWQ@networks.wqx1t.mongodb.net")
+
+
+def get_collection_size(onlyOscillators=True):
+    if onlyOscillators:
+        results = collection.find({'oscillator': True})
+        return results.count()
+    else:
+        return collection.count()
+
+
+def get_random_oscillator(n=1):
+    count = get_collection_size()
+    return collection.find()[randrange(count)]
+
+def load_lines(path):
+    '''
+    Load an antimony model from a local machine split into lines
+    :param path: path to the .ant file
+    :return: Returns the antimony test as a list of strings. Omits the first line if it is a comment.
+    '''
+    with open(path, "r") as f:
+        ant = f.read()
+        f.close()
+    lines = ant.split('\n')
+    # First line is comment for fitness, ignore
+    if lines[0].startswith('#'):
+        lines = lines[1:]
+    return lines
+
+
+def load_antimony(path):
+    '''
+    Load an antimony model from a local machine split into lines
+    :param path: path to the .ant file
+    :return: a single antimony string for the model. Includes all lines including comments.
+    '''
+    # THIS WILL INCLUDE THE FIRST COMMENTED LINE!
+    with open(path, "r") as f:
+        ant = f.read()
+        f.close()
+    return ant
+
+
+def get_nReactions(ant):
+    '''
+    Count how many reactions are in the model
+    :param ant: antimony strings split by line, usually loaded with load_lines
+    :return: integer, number of reactions
+    '''
+    # Takes a list of strings for each line in ant file
+    nReactions = 0
+    for line in ant:
+        if '->' in line and not line.startswith('#'):
+            if line.startswith('k'):
+                break
+            nReactions += 1
+    return nReactions
+
+
+def get_nNodes(ant):
+    nNodes = 0
+    # Skip the first line if it is a comment
+    if ant[0].startswith('#'):
+        ant = ant[1:]
+    for line in ant:
+        if line.startswith('var') or line.startswith('ext'):
+            nNodes += 1
+        else:
+            break
+    return nNodes
+
+
+def query_database(query):
+    '''
+    Retrieve all entries that match the query
+    :param query: A dictionary of the desired model traits
+    :return: A cursor object containing the dictionaries for all matching models
+    '''
+    cur = collection.find(query)
+    print(f'Found {cur.count()} matching entries.')
+    return collection.find(query)
+
+
+def get_ids(query):
+    '''
+    Get the IDs of models that match the query
+    :param query: A dictionary of model traits to look for
+    :return: A list of IDs (str) for the matching models
+    '''
+    doc = collection.find(query)
+    result = []
+    for x in doc:
+        result.append(x['ID'])
+    if len(result) == 0:
+        print('No entries found.')
+        return None
+    return result
+
+def update_model(query, update):
+    newValue = {"$set" : update}
+    collection.update_one(query, newValue)
+
+
+def get_model_by_id(id):
+    # If the id is provided as an integer, convert to string
+    if isinstance(id, int):
+        id = str(id)
+    result = collection.find_one({'ID': id})
+    if not result:
+        print(f'Model {id} not found.')
+    else:
+        return result
+
+
+def get_antimony(query):
+    '''
+    Get the antimony string(s) of models that match the query
+    :param query: A dictionary of model traits to look for
+    :return: If there is only a single matching model, returns the string for that model.
+             Otherwise, returns a list of model strings.
+    '''
+    doc = collection.find(query)
+    result = []
+    for x in doc:
+        result.append(x['model'])
+    if len(result) == 0:
+        print('No entries found.')
+    elif len(result) == 1:
+        return result[0]
+    else:
+        return result
+
+
+def yes_or_no(question):
+    '''
+    Prompt the user to answer a yes or no question
+    :param question: The question to be asked (str)
+    :return: True if yes, False if no (boolean)
+    '''
+    answered = False
+    while not answered:
+        reply = str(input(question + ' (y/n): ')).lower().strip()
+        if reply[0] == 'y':
+            return True
+        elif reply[0] == 'n':
+            return False
+        else:
+            print('Please answer y or n.')
+
+
+def add_many(path, oscillator, massConserved, num_nodes=None):
+    '''
+    Add several antimony models to the database from a local folder containing .ant files.
+    :param path: Path to the folder where the antimony models are located.
+    :param oscillator: Oscillation status. True for oscillator, False for non oscillator (booleans), or 'damped' (str)
+    :return: None
+    '''
+    if not os.path.exists(path):
+        raise ValueError("Invalid path")
+    modelList = []
+    os.chdir(path)
+    for filename in os.listdir(path):
+        if not filename.endswith('.ant') or os.path.isdir(filename):
+            continue
+        ant_lines = load_lines(filename)
+        nReactions = get_nReactions(ant_lines)
+        if not num_nodes:
+            nNodes = get_nNodes(ant_lines)
+        else:
+            nNodes = num_nodes
+        ant = load_antimony(filename)
+
+        if filename.startswith('FAIL_Model_'):
+            ID = filename[11:-4]
+        elif filename.startswith('Model_'):
+            ID = filename[6:-4]
+        elif filename.endswith('.ant'):
+            ID = filename[:-4]
+        else:
+            ID = filename
+        if not (oscillator == True or oscillator == False or oscillator == 'damped'):
+            raise ValueError("Oscillator argument must be True, False, or 'damped'")
+        modelDict = {'ID': ID,
+                     'num_nodes': nNodes,
+                     'num_reactions': nReactions,
+                     'model': ant,
+                     'oscillator': oscillator,
+                     'mass_conserved' : massConserved}
+        modelList.append(modelDict)
+    collection.insert_many(modelList)
+    print(f"Successfully added {len(modelList)} models to database")
+
+
+def add_ant_extension(path):
+    os.chdir(path)
+    count = 0
+    for filename in os.listdir(path):
+        os.rename(filename, filename[:-4])
+        count += 1
+    print(f'Successfully added .ant extension to {count} files')
+
+
+def delete_by_query(query, yesNo=True):
+    '''
+    Delete models that match the query from the database
+    :param query: A dictionary with the target traits of models to be deleted.
+    :return: None
+    '''
+    if query == {}:
+        print('Deleting the entire database is not allowed.')
+        return None
+    if yesNo:
+        proceed = yes_or_no(f"Are you sure you want to delete all models that math the query {str(query)}?")
+        if proceed:
+            collection.delete_many(query)
+            print(f"Successfully deleted models matching the query {str(query)}")
+    else:
+        collection.delete_many(query)
+
+
+
+def delete_by_path(path):
+    '''
+    Delete models from the database that match models in a local folder.
+    This is useful if models from a local folder were incorrectly or inadvertently added to the database.
+    :param path: Path to local folder where the .ant files are located.
+    :return: None
+    '''
+    if not os.path.exists(path):
+        raise ValueError('Invalid path.')
+    os.chdir(path)
+    count = 0
+    for filename in os.listdir(path):
+        if filename.endswith('.ant'):
+            if filename.startswith('FAIL_Model_'):
+                ID = filename[11:-4]
+            elif filename.startswith('Model_'):
+                ID = filename[6:-4]
+            else:
+                ID = filename[-4]
+            collection.delete_one({'ID': ID})
+            count += 1
+    print(f"Successfully deleted {count} models from the database.")
+
+
+def get_sbml(query, sbml_path):
+    id_list = get_ids(query)
+    if not os.path.exists(sbml_path) or not os.path.isdir(sbml_path):
+        os.mkdir(sbml_path)
+    total = len(id_list)
+    count = 0
+    for id in id_list:
+        try:
+            ant = get_antimony({"ID": id})
+            r = te.loada(ant)
+            r.exportToSBML(f"{os.path.join(sbml_path, id)}.sbml")
+            count += 1
+        except:
+            continue
+    print(f"Exported {count} of {total} models to {sbml_path}")
+
+def findMisProcessed(antimony):
+    lines = antimony.splitlines()
+    speciesList = []
+    for line in lines:
+        if line.startswith('var') or line.startswith('ext'):
+            if line not in speciesList:
+                speciesList.append(line)
+            else:
+                return True, antimony
+    return False, None
+
+
+def deleteBadModels(query, writeOut=False, destinationPath=None):
+    results = mm.query_database(query)
+    processed = 0
+    count = 0
+    if destinationPath:
+        os.chdir(destinationPath)
+    for model in results:
+        isMisProcessed, antimony = findMisProcessed(model['model'])
+        if isMisProcessed:
+            count += 1
+            mm.delete_by_query({'ID': model['ID']}, yesNo=False)
+        processed += 1
+        if writeOut:
+            with open(model['ID']+'.ant', "w") as f:
+                f.write(model['model'])
+                f.close()
+    print(f'Deleted {count} of {processed} models :(')
+
+
+
+get_connection()
