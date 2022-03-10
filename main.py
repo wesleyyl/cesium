4cb173c2aba676d0b9e51a7abf2a9d5cfdefbf75
Wesley <wesleyluk.111@gmail.com>
2022-03-09 22:35:27 -0800

    Download restructured w/ bytesio, removed download folder, bootstrap visual updates w/ buttons & containers

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 #Flask Packages
-from flask import Flask, request, render_template, url_for, send_from_directory
-from flask import Response
-#from flask import redirect
+from flask import Flask, request, render_template, url_for, send_file, Response
+#from flask import redirect, make_response, send_from_directory
 
 #mongoMethods Dependencies
 import pymongo
@@ -11,7 +10,10 @@ import oscillatorDB.mongoMethods as mm
 
 #Miscellaneous Packages
 from zipfile import ZipFile
+from io import BytesIO
 import os
+import json #to convert string to dictionary
+# import base64
 
 
 #Initializing Flask app and mongoMethods Startup
@@ -21,49 +23,18 @@ mm.startup()
 
 #Config Values
 app.config["DOWNLOAD_FOLDER"] = 'download'
-app.config["FILENAME"] = "download.zip"
+app.config["ZIPF_NAME"] = "download.zip"
 
 
 #Index Page
 @app.route("/")
 def index():
-    
     return render_template('index.html')
 
-
-    # num_nodes = request.args.get("num_nodes", "")
-    # num_reactions = request.args.get("num_reactions", "")
-    # oscillator = request.args.get("oscillator", "")
-    # autocatalysis = request.args.get("autocatalysis", "")
-    # degradation = request.args.get("degradation", "")
-
-    # try:
-    #   num_nodes = int(num_nodes)
-    #   num_reactions = int(num_reactions)
-    # except ValueError:
-    #   pass
-
-    # # num_nodes = 3
-    # # num_reactions = 5
-    # # WHY ARE THESE 2 VALUES NOT BEING PASSED/RETURNED?!
-
-    # if oscillator == "osc_yes":
-    #   oscillator_status = True
-    # elif oscillator == "osc_no":
-    #   oscillator_status = False
-    # else:
-    #   oscillator_status = -1; #default value when website first loaded
-
-    # result = oscillatorDB(num_nodes, num_reactions, oscillator_status)
-
-    # if result[0] == "Done":
-    #   return render_template('download.html', value=result[1])
-    #   #return redirect('/download/' + app.config["FILENAME"])
-    # elif result == "No entries found":
-    #   return render_template('index.html', value=result[0])
-    # else:
-    #   return render_template('index.html', value="")
-
+#Download Transition Page
+# 
+# This route will take form data and convert parameters into a query dictionary.
+# 
 @app.route("/download", methods = ['GET', 'POST'])
 def download():
   if request.method == 'GET':
@@ -94,6 +65,8 @@ def download():
       oscillator_status = True
     elif oscillator == "osc_no":
       oscillator_status = False
+    else:
+      oscillator_status = False
 
     #IF AUTOCATALYSIS LEFT EMPTY IT DEFAULTS TO NO!
     if autocatalysis == "Y/N":
@@ -103,9 +76,6 @@ def download():
     if degradation == "Y/N":
       degradation = False
 
-    
-    status = oscillatorDB(num_nodes, num_reactions, oscillator_status)
-
     queryParam = {
       'nodes' : num_nodes,
       'reac' : num_reactions,
@@ -114,13 +84,16 @@ def download():
       'degrade' : degradation
     }
 
-    # return render_template('download.html', filename=app.config["FILENAME"])
+    # cesiumZip = cesiumQuery(queryParam)
+
+    # encoded_zipF = base64.b64encode(cesiumZip.getvalue())
+
 
     #DEBUG CODE: TO LOAD PAGE WITH QUERY VALUES UNDERNEATH
-    if status == 1:
-      return render_template('download.html', filename=app.config["FILENAME"], query=queryParam)
-    elif status == 0:
-      error = "No models found. Please try something different."
+    if cesiumQueryExists(queryParam):
+      return render_template('download.html', query=json.dumps(queryParam))
+    elif not cesiumQueryExists(queryParam):
+      error = "No models found. Please try a different query."
       return render_template('error.html', error_msg=error)
     elif status == -1:
       error = "Invalid inputs. Please try again."
@@ -131,25 +104,110 @@ def download():
     #DEBUG CODE: TO CHECK THAT SPECIFIC VALUE IS CORRECT
     # return str(num_nodes)
 
-@app.route("/download/<filename>")
-def download_file(filename):
-  #filepath = os.path.join(app.static_folder, app.config["FILENAME"])
-  #filepath = app.static_folder
 
-  filepath = os.path.join(app.config["DOWNLOAD_FOLDER"], app.config["FILENAME"])
+#Download File Page
+@app.route("/download/<query>")
+def download_file(query):
+  # filepath = os.path.join(app.config["DOWNLOAD_FOLDER"], app.config["ZIPF_NAME"])
+  filename = app.config["ZIPF_NAME"]
   
-  # @app.after_request
-  # def delete_file(response):
-  #   try:
-  #     os.remove(filepath)
-  #   except Exception as error:
-  #     app.logger.error("Error removing downloaded file handle", error)
-  #   return response
+  # return query
 
-  try:
-    return send_from_directory(app.config["DOWNLOAD_FOLDER"], filename, as_attachment=True)
-  except FileNotFoundError:
-    abort(404)
+  cesiumZip = cesiumQuery(json.loads(query))
+
+  return send_file(cesiumZip, attachment_filename=filename, as_attachment=True)
+
+  # try:
+
+    #Method 1
+    # with open(filepath, 'rb') as zipFile:
+    #   data = zipFile.readlines()
+    # os.remove(filepath)
+    # 
+    # return Response(data, headers={
+    #   'Content-Type': 'application/zip',
+    #   'Content-Disposition': 'attachment; filename=%s;' % app.config["ZIPF_NAME"]
+    #   })
+
+
+    #Method 2
+    # response = make_response(file.read())
+    # response.headers.set('Content-Type', 'zip')
+    # response.headers.set('Content-Disposition', 'attachment', filename='%s.zip' % app.config["ZIPF_NAME"])
+
+    # return response
+
+
+
+    #Method 3
+    # return Response(file, mimetype='application/zip', headers={'Content-Disposition': 'attachment;filename={}'.format(filename)})
+
+
+    #Method 4
+    # return send_file(cesiumZip, attachment_filename=app.config["ZIPF_NAME"], as_attachment=True)
+  # except FileNotFoundError:
+  #   abort(404)
+
+
+#About Page
+@app.route("/about")
+def about():
+  return render_template('about.html')
+
+
+#FAQ Page
+@app.route("/faq")
+def faq():
+  return render_template('faq.html')
+
+
+def cesiumQueryExists(query):
+  query = { "num_nodes" : query["nodes"], "num_reactions" : query["reac"], "oscillator" : query["osc"] }
+  model_IDS = mm.get_ids(query)
+
+  if model_IDS:
+    return True
+  else:
+    return False
+
+def cesiumQuery(query):
+
+  # filepath = os.path.join(app.config["DOWNLOAD_FOLDER"], app.config["ZIPF_NAME"])
+
+  query = { "num_nodes" : query["nodes"], "num_reactions" : query["reac"], "oscillator" : query["osc"] }
+  model_IDS = mm.get_ids(query)
+  
+  if model_IDS:
+    memZipFile = BytesIO()
+    with ZipFile(memZipFile, "w") as zipF:
+      for ID in model_IDS:
+        antStr = mm.get_antimony({ "ID" : ID })
+        txtFilename = str(ID) + ".txt"
+        zipF.writestr(txtFilename, antStr)
+    memZipFile.seek(0)
+
+    return memZipFile
+
+        
+  else:
+    return None
+
+
+# def createTextFile(antStr, txtFileName):
+#   txtFile = io.BytesIO()
+#   with open(txtFileName, "wb") as txtF:
+#     txtF.write(antStr)
+
+
+
+# def addToZip(memZip, txtFileName, antStr): #simplify by removing zipfilename parameter
+#     with zipfile.ZipFile(zip, "w", zipfile.ZIP_DEFLATED) as zipF:
+#       zipF.writestr(txtFileName, antStr)
+
+
+
+
+
 
 
 
@@ -159,44 +217,35 @@ def download_file(filename):
 # return 0: no models found using that query
 # return 1: query successful
 # 
-def oscillatorDB(num_nodes, num_reactions, oscillator):
+# def oscillatorDB(num_nodes, num_reactions, oscillator):
 
-  filepath = os.path.join(app.config["DOWNLOAD_FOLDER"], app.config["FILENAME"])
-  if os.path.exists(filepath):
-    os.remove(filepath)
+#   filepath = os.path.join(app.config["DOWNLOAD_FOLDER"], app.config["FILENAME"])
+#   if os.path.exists(filepath):
+#     os.remove(filepath)
 
-  def createToZipFile(zipfilename, filename, antimony_model): #simplify by removing zipfilename parameter
-    with ZipFile(filepath, "a") as zip_file:
-      zip_file.writestr(filename, antimony_model)
-
-
-  try:
-    query = { "num_nodes" : num_nodes, "num_reactions" : num_reactions, "oscillator" : oscillator }
-    model_IDS = mm.get_ids(query)
-
-    # createZipFile("download.zip")
-    if model_IDS:
-      for ID in model_IDS:
-        ant = mm.get_antimony({ "ID" : ID })
-        filename = str(ID) + ".txt"
-        createToZipFile(app.config["FILENAME"], filename, ant) #can simplify by removing first parameter
-      return 1
-    else:
-      return 0
-
-  except ValueError:
-    return -1
+#   def createToZipFile(zipfilename, filename, antimony_model): #simplify by removing zipfilename parameter
+#     with ZipFile(filepath, "a") as zip_file:
+#       zip_file.writestr(filename, antimony_model)
 
 
+#   try:
+#     query = { "num_nodes" : num_nodes, "num_reactions" : num_reactions, "oscillator" : oscillator }
+#     model_IDS = mm.get_ids(query)
+
+#     # createZipFile("download.zip")
+#     if model_IDS:
+#       for ID in model_IDS:
+#         ant = mm.get_antimony({ "ID" : ID })
+#         filename = str(ID) + ".txt"
+#         createToZipFile(app.config["FILENAME"], filename, ant) #can simplify by removing first parameter
+#       return 1
+#     else:
+#       return 0
+
+#   except ValueError:
+#     return -1
 
 
-@app.route("/about")
-def about():
-  return render_template('about.html')
-
-@app.route("/faq")
-def faq():
-  return render_template('faq.html')
 
 #Flask Development Server
 if __name__ == "__main__":
